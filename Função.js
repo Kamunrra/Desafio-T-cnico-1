2a21a2fb5dae31f464c7312fc50a5739a156f645
Giovanne <136638112+Kamunrra@users.noreply.github.com>
2023-11-23 13:07:07 -0300

    Update Função.js

--- Função.js
+++ Função.js
@@ -1,3 +1,10 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 function somatorioDivisiveis(numero) {
   let soma = 0;
   for (let i = 1; i < numero; i++) {
@@ -7,3 +14,20 @@ function somatorioDivisiveis(numero) {
   }
   return soma;
 }
+
+function calcularSomatorio() {
+  rl.question('Informe um número inteiro positivo: ', (numero) => {
+    const parsedNumero = parseInt(numero);
+
+    if (isNaN(parsedNumero) || parsedNumero <= 0) {
+      console.log('Por favor, informe um número inteiro positivo.');
+      rl.close();
+    } else {
+      const resultado = somatorioDivisiveis(parsedNumero);
+      console.log(`Resultado: ${resultado}`);
+      rl.close();
+    }
+  });
+}
+
+calcularSomatorio();
